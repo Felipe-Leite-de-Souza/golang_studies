ab41801919793268fc46b777d5cbc7cf9a7888a3
fleitedeso <felipe_leitesouza@hotmail.com>
2022-12-15 08:18:26 -0300

    Exemplo de pacotes
    Signed-off-by: fleitedeso <felipe_leitesouza@hotmail.com>

---
+++ 1-Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Função que escreve uma mensagem na tela - vista por outros pacotes
+func Escrever() {
+	fmt.Println("Hello Word Auxiliar!")
+	escrever2()
+}
---
+++ 1-Pacotes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
+)
+
+func main() {
+	fmt.Println("Hello Word!")
+	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste@gmail.com")
+	fmt.Println(erro)
+}
