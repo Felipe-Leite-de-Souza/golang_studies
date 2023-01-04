15a290a25106bfbd09842c2e79aefce9dd6e1c0e
fleitedeso <felipe_leitesouza@hotmail.com>
2023-01-04 10:27:40 -0300

    atualizacao dos testes unitarios
    Signed-off-by: fleitedeso <felipe_leitesouza@hotmail.com>

--- 32-TestesAutomatizados/32.1-Introducao/endereco/endereco_test.go
+++ 32-TestesAutomatizados/32.1-Introducao/endereco/endereco_test.go
@@ -4,13 +4,35 @@ package endereco
 
 import "testing"
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 func TestTipoDeEndereco(t *testing.T) {
-	enderecoParaTeste := "Rua ABC"
 
-	tipoDeEnderecoEsperado := "Rua"
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Estrada ABC", "Estrada"},
+		{"Praça das Rpsas", "Tipo inválido"},
+		{"RUA Teste", "Rua"},
+		{"AVENIDA Teste", "Avenida"},
+		{"", "Tipo inválido"},
+	}
 
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("O tipo é diferente do esperado! Esperava %s e recebeu %s", tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	//enderecoParaTeste := "Rua ABC"
+	//tipoDeEnderecoEsperado := "Rua"
+	//tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
+	//if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
+	//	t.Errorf("O tipo é diferente do esperado! Esperava %s e recebeu %s", tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	//}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", tipoDeEnderecoRecebido, cenario.retornoEsperado)
+		}
 	}
 }
